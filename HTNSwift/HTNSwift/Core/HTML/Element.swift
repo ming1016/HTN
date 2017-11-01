9da3232dfe6b032eeb4d912d87d8dce849b2bfdc
sunshinelww <liweiweisunshinelww@didichuxing.com>
2017-11-01 12:57:25 +0800

    实现初步的HTML装Texture

--- HTNSwift/HTNSwift/Core/HTML/Element.swift
+++ HTNSwift/HTNSwift/Core/HTML/Element.swift
@@ -12,9 +12,12 @@ public class Element : Node {
     public var startTagToken: HTMLToken?
     public var charToken: HTMLToken?
     public var propertyMap = [String:String]() //属性映射表
+    public var renderer:RenderObject? //代表元素的渲染对象
+    
     override init() {
         
     }
+    
     init(token: HTMLToken) {
         
         if token.type == .StartTag {
@@ -23,5 +26,9 @@ public class Element : Node {
         if token.type == .Char {
             self.charToken = token
         }
+    }
+    
+    public func createRenderObject (){
+        renderer=RenderObject();
     }
 }
--- HTNSwift/HTNSwift/Core/Layout/RenderObject.swift
+++ HTNSwift/HTNSwift/Core/Layout/RenderObject.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+public class RenderObject {
+    
+    public var x : Double? //代表元素的一个布局位置，暂时只考虑这四个属性，以后再添加
+    public var y : Double?
+    public var width  : Double
+    public var height : Double
+    
+    //主要设置盒子模型的padding属性
+    public var padding_top : Double = 0
+    public var padding_left :Double = 0
+    public var padding_bottom : Double = 0
+    public var padding_right : Double = 0
+    
+    //盒子模型的border属性
+    public var border_top : Double = 0
+    public var border_left : Double = 0
+    public var border_bottom : Double = 0
+    public var border_right : Double = 0
+    
+    //盒子模型的marginInfo,注意父节点的marginInfo需要根据所有子节点信息的marginInfo来共同决定
+    public var margin_top : Double = 0
+    public var margin_left : Double = 0
+    public var margin_bottom : Double = 0
+    public var margin_right : Double = 0
+    
+    public var backgroundColor : CGColor? //背景颜色
+    public var borderColor :CGColor? //边框颜色
+    
+    init() {
+        width=0
+        height=0
+    }
+    
+    public func setPadding(_ top :Double, _ left : Double, _ bottom: Double, _ right: Double){
+        padding_top = top
+        padding_left = left
+        padding_bottom = bottom
+        padding_right = right
+    }
+}
