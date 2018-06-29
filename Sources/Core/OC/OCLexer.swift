baf37ce8bdde7737077f66d4657b2fe78e7c05dd
ming1016 <ming1016@foxmail.com>
2018-06-29 14:08:53 +0800

    arrange

--- Sources/Core/OC/OCLexer.swift
+++ Sources/Core/OC/OCLexer.swift
@@ -6,3 +6,111 @@
 //
 
 import Foundation
+
+public class OCLexer {
+    private let text: String
+    private var currentIndex: Int
+    private var currentCharacter: Character?
+    
+    public init(_ input: String) {
+        if input.count == 0 {
+            fatalError("Error! input can't be empty")
+        }
+        self.text = input
+        currentIndex = 0
+        currentCharacter = text[text.startIndex]
+    }
+    
+    // 流程函数
+    func nextTk() -> OCToken {
+        if currentIndex > self.text.count - 1 {
+            return .eof
+        }
+        
+        if CharacterSet.whitespacesAndNewlines.contains((currentCharacter?.unicodeScalars.first!)!) {
+            skipWhiteSpaceAndNewLines()
+            return .whiteSpaceAndNewLine
+        }
+        
+        if CharacterSet.decimalDigits.contains((currentCharacter?.unicodeScalars.first!)!) {
+            return number()
+        }
+        
+        if currentCharacter == "+" {
+            advance()
+            return .operation(.plus)
+        }
+        if currentCharacter == "-" {
+            advance()
+            return .operation(.minus)
+        }
+        if currentCharacter == "*" {
+            advance()
+            return .operation(.mult)
+        }
+        if currentCharacter == "/" {
+            advance()
+            return .operation(.intDiv)
+        }
+        if currentCharacter == "(" {
+            advance()
+            return .paren(.left)
+        }
+        if currentCharacter == ")" {
+            advance()
+            return .paren(.right)
+        }
+        if currentCharacter == "@" {
+            
+        }
+        advance()
+        return .eof
+    }
+    // @符号的处理
+    private func at() -> OCToken {
+        advance()
+        var atStr = ""
+        while let character = currentCharacter,  CharacterSet.whitespacesAndNewlines.contains((character.unicodeScalars.first!)) {
+            atStr += String(character)
+            advance()
+        }
+        if atStr == "interface" {
+            return .atInterface
+        }
+        fatalError("Error: at string not support")
+    }
+    // 数字处理
+    private func number() -> OCToken {
+        var numStr = ""
+        while let character = currentCharacter,  CharacterSet.decimalDigits.contains((character.unicodeScalars.first!)) {
+            numStr += String(character)
+            advance()
+        }
+        return .constant(.integer(Int(numStr)!))
+    }
+    
+    // 辅助函数
+    private func advance() {
+        currentIndex += 1
+        guard currentIndex < text.count else {
+            currentCharacter = nil
+            return
+        }
+        currentCharacter = text[text.index(text.startIndex, offsetBy: currentIndex)]
+    }
+    
+    // 往前探一个字符，不改变当前字符
+    private func peek() -> Character? {
+        let peekIndex = currentIndex + 1
+        guard peekIndex < text.count else {
+            return nil
+        }
+        return text[text.index(text.startIndex, offsetBy: peekIndex)]
+    }
+    
+    private func skipWhiteSpaceAndNewLines() {
+        while let character = currentCharacter, CharacterSet.whitespacesAndNewlines.contains((character.unicodeScalars.first!)) {
+            advance()
+        }
+    }
+}
--- Sources/Core/OC/OCToken.swift
+++ Sources/Core/OC/OCToken.swift
@@ -6,3 +6,104 @@
 //
 
 import Foundation
+
+public enum OCValue {
+    case none
+    case number(OCNumber)
+    case boolean(Bool)
+    case string(String)
+}
+
+public enum OCConstant {
+    case integer(Int)
+    case float(Float)
+    case boolean(Bool)
+    case string(String)
+}
+
+public enum OCOperation {
+    case plus
+    case minus
+    case mult
+    case intDiv
+}
+
+public enum OCDirection {
+    case left
+    case right
+}
+
+public enum OCToken {
+    case constant(OCConstant)
+    case operation(OCOperation)
+    case paren(OCDirection)
+    case atInterface
+    case eof
+    case whiteSpaceAndNewLine
+}
+
+extension OCConstant: Equatable {
+    public static func == (lhs: OCConstant, rhs: OCConstant) -> Bool {
+        switch (lhs, rhs) {
+        case let (.integer(left), .integer(right)):
+            return left == right
+        case let (.float(left), .float(right)):
+            return left == right
+        case let (.boolean(left), .boolean(right)):
+            return left == right
+        case let (.string(left), .string(right)):
+            return left == right
+        default:
+            return false
+        }
+    }
+}
+
+extension OCOperation: Equatable {
+    public static func == (lhs: OCOperation, rhs: OCOperation) -> Bool {
+        switch (lhs, rhs) {
+        case (.plus, .plus):
+            return true
+        case (.minus, .minus):
+            return true
+        case (.mult, .mult):
+            return true
+        case (.intDiv, .intDiv):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+extension OCDirection: Equatable {
+    public static func == (lhs: OCDirection, rhs: OCDirection) -> Bool {
+        switch (lhs, rhs) {
+        case (.left, .left):
+            return true
+        case (.right, .right):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+extension OCToken: Equatable {
+    public static func == (lhs: OCToken, rhs: OCToken) -> Bool {
+        switch (lhs, rhs) {
+        case let (.constant(left), .constant(right)):
+            return left == right
+        case let (.operation(left), .operation(right)):
+            return left == right
+        case (.eof, .eof):
+            return true
+        case (.whiteSpaceAndNewLine, .whiteSpaceAndNewLine):
+            return true
+        case let (.paren(left), .paren(right)):
+            return left == right
+        default:
+            return false
+        }
+    }
+}
